b15064b65589637e802f16ca42bfcc80d643b8cb
NicoNelli <nicolobastianelli@gmail.com>
2019-01-25 15:43:56 +0100

    updated callbackHandler for roll&pitch&yaw

--- include/common/CallbackHandler.hpp
+++ include/common/CallbackHandler.hpp
@@ -5,6 +5,7 @@
 #include "MavState.h"
 #include "lcm/lcm-cpp.hpp"
 #include "lcm_messages/geometry/pose.hpp"
+#include "lcm_messages/geometry/vision.hpp"
 #include "lcm_messages/geometry/UltrasonicPosition.hpp" //for the ultrasonic message
 #include "lcm_messages/exec/task.hpp"
 #include "lcm_messages/exec/state.hpp"
@@ -153,7 +154,7 @@ public:
      * @details It sets the position, velocity, orientation 
      */
 
-	void ApriltagCallback(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const geometry::pose* msg){
+	void ApriltagCallback(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const geometry::vision* msg){
 	//callback of the vision system.
 
 		_relative_pos.setPosition((float)msg->position[0],(float)msg->position[1],(float)msg->position[2]);
@@ -161,7 +162,12 @@ public:
     	_relative_pos.setV((float)msg->velocity[0],(float)msg->velocity[1],(float)msg->velocity[2]);
 
     	_relative_pos.setOrientation((float)msg->orientation[0],(float)msg->orientation[1],(float)msg->orientation[2],(float)msg->orientation[3]);
-
+	
+		_relative_pos.setRoll( (float)msg->roll );
+		_relative_pos.setPitch( (float)msg->pitch );
+		_relative_pos.setYaw( (float)msg->yaw );
+		
+	
 	}
 
     /**

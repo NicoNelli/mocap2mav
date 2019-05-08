b9aa3f29f99674bcca8ba9254f3f9d4ffad62be4
NicoNelli <nicolobastianelli@gmail.com>
2019-05-08 17:12:59 +0200

    added static params

---
+++ modules/Automatic/include/param.h
@@ -0,0 +1,115 @@
+//
+// Created by andreanistico on 21/02/17.
+//
+
+#ifndef MOCAP2MAV_PARAM_H
+#define MOCAP2MAV_PARAM_H
+
+/** @ingroup Automatic
+ *  List of parameters used for the Landing procedure
+ */
+
+namespace params_automatic {
+
+	//roll upper limit to land
+	static const double RollThreshold = 0.1;    
+
+	//pitch upper limit to land
+	static const double PitchThreshold = 0.1;
+
+	/**
+     * Offset between baricenter of the drone and the ultrasonic sensor 
+     */
+
+	//static const double OffsetUltraSensor = 0.1; // respect to the baricenter of the drone
+
+	static const double OffsetUltraSensor = -0.082; //respect to the robot legs
+
+    /**
+     * Number of consecutive frames in which tracking is considered valid 
+     */
+
+    static const int    NFramesHold     = 120;
+
+    /**
+     * Number of consecutive frames in which tracking is considered not valid 
+     */
+
+    static const int    NFramesLost     = 70;
+
+    /**
+     * Number of consecutive frames in which tracking is considered valid
+     * and robot is ready for compensation
+     */
+
+    static const int    NFramesComp     = 20;
+
+    /**
+     * Platform dimension 
+     */
+
+    static const double platformLenght  = 0.65;
+
+    /**
+     * Max altitude for landing procedure 
+     */
+
+    static const double zMax            = 1.80;
+
+    /**
+     * Minimum altitude for landing procedure (before compensating)     
+     */
+
+    static double       zMin            = 0.5;
+
+    /**
+     * Proportional gain times platform velocity 
+     */
+
+    static const double KpHoldV          = 1;
+
+    /**
+     * Proportional gain times horizontal error 
+     */
+
+    static const double KpHold           = 0.4; //0.7;//0.5;//0.78; 
+
+    /**
+     * Differential gain times horizontal error 
+     */
+
+    static const double KdHold           = 0.00;
+
+    /**
+     * Integral gain times integral horizontal error 
+     */
+
+    static const double KiHold          = 0.001; //0.1
+
+    /**
+     * Proportional gain for velocity tracking 
+     */
+
+    static const double KPCompV          = 0.1;
+
+    /**
+     * Integral clamping values 
+     */
+
+    static const double maxIntValue     = 1;
+    
+    /**
+     * Integral clamping values
+     */
+
+    static const double minIntValue     = -maxIntValue;
+
+    /**
+     * Max total PID output 
+     */
+
+    static const double maxOutput       = 1.5;
+
+}
+
+#endif //MOCAP2MAV_PARAM_H
